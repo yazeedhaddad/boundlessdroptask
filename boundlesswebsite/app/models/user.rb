01ed04badd550b81bdf1f8d0a952faeddc77bb48
yazeedhaddad <yazeed.b.haddad@gmail.com>
2016-12-05 16:16:13 +0200

    sign up to boundlessdrop website

--- boundlesswebsite/app/models/user.rb
+++ boundlesswebsite/app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+	attr_accessor :password
+	#attr_accessible :name, :email
+	#validates :password, presence:true,lenght:{minimum:6}
+	#validates :email, presence:true
+	validates :password, presence: true
+	validates :email, presence: true
+	validates :name, presence: true
+
+
+def self.authenticate(email,password)
+	user=find_by_email(email)
+	password=find_by_password(password)
+	return nil if user.nil?
+	return user unless password.nil?
+	end
 end
